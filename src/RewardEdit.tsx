285219f09d4acc4e3b7b9a4e18fef2ab68e15bd5
chen min <chenmin4ever@163.com>
2024-06-23 17:10:54 +0700

    去除无用代码，消除部分警告

--- src/RewardEdit.tsx
+++ src/RewardEdit.tsx
@@ -1,5 +1,5 @@
 // EmployeeEdit.tsx
-import React, { useState } from "react";
+import { useState } from "react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 
